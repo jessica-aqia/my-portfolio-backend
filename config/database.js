5494209ef9b2653a013b19663d041a4628410f7c
jessica-aqia <jihsuan0215@gmail.com>
2025-09-02 00:54:47 +0800

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -5,6 +5,7 @@ const dbConfig = {
   user: process.env.DB_USER || "your_username",
   password: process.env.DB_PASSWORD || "your_password",
   database: process.env.DB_NAME || "selfpalette_projects",
+  port: process.env.DB_PORT || 3306,
 };
 
 module.exports = dbConfig;
